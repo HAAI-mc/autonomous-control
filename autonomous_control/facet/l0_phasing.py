758aa85c950927e6a625bb6001e3be11314c1fa2
Ryan Roussel <rroussel@slac.stanford.edu>
2026-06-30 15:40:53 -0500

    refactor to use set_variables, get_variables from env

--- autonomous_control/facet/l0_phasing.py
+++ autonomous_control/facet/l0_phasing.py
@@ -1,10 +1,10 @@
 import sys
+import logging
 from time import sleep
 from datetime import datetime
 import numpy as np
 from matplotlib import pyplot as plt
 
-from epics import get_pv
 sys.path.append('/usr/local/facet/tools/python/')
 from F2_pytools.f2bsaBuffer import f2BeamSynchronousBuffer
 
@@ -44,7 +44,7 @@ def l0_phasing(
     -------
     None
     """
-    scandata = fast_phase_scan_l0(k=k, p0=p0, pf=pf, Nshots=Nshots)
+    scandata = fast_phase_scan_l0(env=env, k=k, p0=p0, pf=pf, Nshots=Nshots)
     scandata = fit_beam_phase(scandata)
     for k,v in scandata.items():
         logger.debug(f'{k}: {v}')
@@ -55,19 +55,20 @@ def l0_phasing(
     else:
         apply_correction = input(f"Measured psi = {scandata['psi_meas']}. Update REFPOC? [y/n]").lower() == 'y'
     if apply_correction:
-        correct_phase_error(scandata)
+        correct_phase_error(env, scandata)
 
-def fast_phase_scan_l0(k, p0, pf, Nshots):
+
+def fast_phase_scan_l0(env, k, p0, pf, Nshots):
     """ fast BSA RF phase scan for L0-A/B only """
     if k not in [4,8]:
         raise ValueError('klys must be 41 (L0A) or 81 (L0B)')
 
-    PV_fbeam       = get_pv(f'EVNT:SYS1:1:BEAMRATE')
-    PV_lfb_control = get_pv(f'PHYS:SYS1:1:F2LFB_DL10E')
-    PV_sfb_control = get_pv(f'KLYS:LI10:{k}1:SFB_PDIS')
-    PV_sfb_pdes    = get_pv(f'KLYS:LI10:{k}1:SFB_PDES')
-    PV_ffb_pdes    = get_pv(f'KLYS:LI10:{k}1:PDES')
-    PV_refpoc      = get_pv(f'KLYS:LI10:{k}1:REFPOC')
+    fbeam_pv = 'EVNT:SYS1:1:BEAMRATE'
+    lfb_control_pv = 'PHYS:SYS1:1:F2LFB_DL10E'
+    sfb_control_pv = f'KLYS:LI10:{k}1:SFB_PDIS'
+    sfb_pdes_pv = f'KLYS:LI10:{k}1:SFB_PDES'
+    ffb_pdes_pv = f'KLYS:LI10:{k}1:PDES'
+    refpoc_pv = f'KLYS:LI10:{k}1:REFPOC'
 
     buf = f2BeamSynchronousBuffer(
         EPICS_address_list=[
@@ -80,10 +81,10 @@ def fast_phase_scan_l0(k, p0, pf, Nshots):
         nowait=True
         )
 
-    beam_rate     = PV_fbeam.get()
-    pdes_init     = PV_ffb_pdes.get()
-    sfb_pdes_init = PV_sfb_pdes.get()
-    poc_init      = PV_refpoc.get()
+    beam_rate = env.get_variables([fbeam_pv])[fbeam_pv]
+    pdes_init = env.get_variables([ffb_pdes_pv])[ffb_pdes_pv]
+    sfb_pdes_init = env.get_variables([sfb_pdes_pv])[sfb_pdes_pv]
+    poc_init = env.get_variables([refpoc_pv])[refpoc_pv]
 
     # calculate FFB PDES scan range based on desired waveguide phase scan range
     poffset = pdes_init - sfb_pdes_init
@@ -91,20 +92,22 @@ def fast_phase_scan_l0(k, p0, pf, Nshots):
 
     # zero klystron phase, let RF/beam feedbacks converge, then disable them
     logger.info('zeroing pdes')
-    PV_sfb_pdes.put(0)
+    env.set_variables({sfb_pdes_pv: 0})
     sleep(3.0)
-    PV_sfb_control.put(0)
-    PV_lfb_control.put(0)
+    env.set_variables({
+        sfb_control_pv: 0,
+        lfb_control_pv: 0,
+    })
 
     # set initial PDES and wait for FFB to settle
     logger.info('initializing scan')
-    PV_ffb_pdes.put(scan_range[0])
+    env.set_variables({ffb_pdes_pv: scan_range[0]})
     sleep(3.0)
 
     # fast scan -- 1 caput per shot
     for phi in scan_range:
-        logger.info(f'setting ffb pdes = {phi: .3f}', end='\r')
-        PV_ffb_pdes.put(phi)
+        logger.info(f'setting ffb pdes = {phi: .3f}')
+        env.set_variables({ffb_pdes_pv: phi})
         sleep(1/beam_rate)
 
     # get BPM and phase readback BSA data
@@ -112,10 +115,12 @@ def fast_phase_scan_l0(k, p0, pf, Nshots):
 
     # re-enable feedbacks, restore initial phase settings
     logger.info('restoring initial settings')
-    PV_sfb_pdes.put(sfb_pdes_init)
-    PV_ffb_pdes.put(pdes_init)
-    PV_sfb_control.put(1)
-    PV_lfb_control.put(1)
+    env.set_variables({
+        sfb_pdes_pv: sfb_pdes_init,
+        ffb_pdes_pv: pdes_init,
+        sfb_control_pv: 1,
+        lfb_control_pv: 1,
+    })
 
     return {
         'klys': k,
@@ -147,10 +152,10 @@ def fit_beam_phase(scandata):
     scandata['B_meas'] = a[2]
     return scandata
 
-def correct_phase_error(scandata):
+def correct_phase_error(env, scandata):
     """ adjust slow feedback REFPOC to correct measured phase error """
     addr = f"KLYS:LI10:{scandata['klys']}1:REFPOC"
-    get_pv(addr).put(scandata['poc_new'])
+    env.set_variables({addr: scandata['poc_new']})
     logger.info(f"{addr} updated: {scandata['poc_init']: .3f} --> {scandata['poc_new']: .3f}")
 
 def plot_scan_result(scandata):
