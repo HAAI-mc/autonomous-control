fec02e196bbc327182a5a601d3d11430686cf154
Ryan Roussel <rroussel@slac.stanford.edu>
2026-09-10 19:06:37 -0500

    Update test_automatic_6d_cuhxr.py

--- autonomous_control/tests/test_automatic_6d_cuhxr.py
+++ autonomous_control/tests/test_automatic_6d_cuhxr.py
@@ -34,7 +34,7 @@ class TestAutomaticSixDCUHXR:
 
         return environment
 
-    @pytest.xfail(reason="Expected to fail due to incomplete VA for lcls inj")
+    @pytest.mark.skip(reason="Expected to fail due to incomplete VA for lcls inj")
     def test_run_automatic_6d_measurement_on_va(self, env):
         config_dir = Path(env.emittance_config_fname).parent
 
