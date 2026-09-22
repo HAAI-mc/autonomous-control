792f58def73526fa8e60b8e470bdbd3b32069f73
nstelter <nstelter@slac.stanford.edu>
2026-09-21 23:33:10 -0700

    test

--- autonomous_control/facet/tcav_phasing.py
+++ autonomous_control/facet/tcav_phasing.py
@@ -205,7 +205,7 @@ def set_tcav_mode_config_and_wait(
         label="mode",
     )
     # NOTE: there are no readbacks for the mode change, so we just wait a few seconds to let the TCAV update
-    while current_mode != target_mode:
+    if current_mode != target_mode:
         logger.info(
             "waiting %s seconds for TCAV to update after mode change",
             TCAV_STATE_CHANGE_WAIT,
